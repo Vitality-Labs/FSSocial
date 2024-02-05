f36e0788b2ae516ec87d9a7fd87dec15c5e3f904
Vitality Labs <davidstearns561@yahoo.com>
2024-02-04 22:48:05 -0500

    Small final changes

--- client/app.js
+++ client/app.js
@@ -61,14 +61,7 @@ angular
           }
         },
         getProfilePicture: function(userId) {
-          // var path = "/uploads/users/" + userId + ".jpg";
           var output = "../../images/default.png";
-          // common.checkIfImageExists(path, function(exists) {
-          //   if (exists) {
-          //     output = "../.." + path;
-          //     return output;
-          //   }
-          // });
           return output;
         },
         clickPost: function(postId, returnUrl = null) {
